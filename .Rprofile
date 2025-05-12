f5f6b0869c91ce4ba61b945e51cab08ec2fa1c5c
mrguyperson <ted.w.hermann@gmail.com>
2025-05-12 16:16:38 +0200

    updated renv and container creation

--- .Rprofile
+++ .Rprofile
@@ -1,1 +1,8 @@
-source("renv/activate.R")
+options(
+  # renv.config.pak.enabled = TRUE,
+  # repos = c(RSPM = "https://packagemanager.posit.co/cran/2025-04-22")
+)
+
+if (file.exists("renv/activate.R")) {
+  source("renv/activate.R")
+}
---
+++ .devcontainer/setup.R
@@ -0,0 +1,20 @@
+options(
+    # renv.config.pak.enabled = TRUE,
+    # repos = c(RSPM = "https://packagemanager.posit.co/cran/2025-04-22")
+ )
+ pkgs <- c(
+    "here",
+    "nx10/httpgd",
+    "tidyverse",
+    "readxl"
+  )
+
+
+# Initialize renv and install packages using pak + DESCRIPTION
+if (!file.exists("renv.lock")) {
+  renv::init()
+  renv::install(pkgs)
+  renv::snapshot()
+} else {
+  renv::restore()
+}
